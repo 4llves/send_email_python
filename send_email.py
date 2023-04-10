25bde3bd6bff417d03266db4fd73d9c705553e66
Jhonata Alves <jhonatab4@gmail.com>
2023-04-10 02:29:27 -0300

    adjust file naming

--- send_email.py
+++ send_email.py
@@ -19,23 +19,32 @@ SMTP_PORT = 587
 # Lê o arquivo excel que contém os diretórios e os e-mails correspondentes
 diretorios_df = pd.read_excel('LIST.xlsx')
 
-# Cria uma lista de dicionários contendo o e-mail e os diretórios correspondentes
+# Cria uma lista contendo o e-mail e os diretórios correspondentes
 destinatarios = []
 for index, row in diretorios_df.iterrows():
     diretorio = row['DIRETORIES']
     email = row['EMAIL']
-    destinatarios.append({'email': email, 'diretorio': diretorio})
+    titulo_do_email = row['TITULO']
+    corpo_do_email = row['CORPO']    
+    destinatarios.append({
+        'email': email,
+        'diretorio': diretorio,
+        'titulo': titulo_do_email,
+        'corpo': corpo_do_email        
+    })
 
 # Percorre cada destinatário na lista e envia os arquivos correspondentes
 for destinatario in destinatarios:
     arquivos = []
     diretorio = destinatario['diretorio']
     email = destinatario['email']
+    titulo = destinatario['titulo']
+    corpo = destinatario['corpo']    
     # Usa glob para encontrar todos os arquivos no diretório que correspondem ao padrão *.pdf
     files = glob.glob(f"{diretorio}\\*.pdf")
     arquivos.extend(files)
 
-    # Cria o DataFrame a partir da lista de dicionários contendo o nome do arquivo e o diretório
+    # Cria o DataFrame contendo o nome do arquivo e o diretório
     df = pd.DataFrame({'arquivo': arquivos, 'diretorio': [diretorio] * len(arquivos)})
 
     # Cria o objeto para a mensagem de e-mail
@@ -46,11 +55,11 @@ for destinatario in destinatarios:
     msg['To'] = email
 
     # Adiciona o assunto da mensagem
-    msg['Subject'] = 'Envio de Arquivos'
+    msg['Subject'] = titulo
 
     # Adiciona o corpo da mensagem
-    body = 'Segue em anexo os arquivos solicitados.'
-    msg.attach(MIMEText(body, 'plain'))
+    body = corpo
+    msg.attach(MIMEText(body, 'plain'))        
 
     # Percorre cada arquivo encontrado no DataFrame e adiciona como anexo
     for index, row in df.iterrows():
@@ -63,7 +72,8 @@ for destinatario in destinatarios:
             att = MIMEBase('application', 'octet-stream')
             att.set_payload(attachment.read())
             encoders.encode_base64(att)
-            att.add_header('Content-Disposition', f'attachment; filename="{row["arquivo"]}"')
+            #att.add_header('Content-Disposition', f'attachment; filename="{row["arquivo"]}"')
+            att.add_header('Content-Disposition', f'attachment; filename="{row["arquivo"].split(os.path.sep)[-1]}"')
             msg.attach(att)
 
     # Envia a mensagem de e-mail
